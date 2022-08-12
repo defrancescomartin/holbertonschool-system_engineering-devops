ccea2b1104893d7f5a2b5ddc4805ecc688f45ecb
defrancescomartin <defrancescoma@gmail.com>
2022-08-12 16:13:16 -0300

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # Install a package using Puppet
 
-exec { 'puppet-lint':
-  command => '/bin/gem install puppet-lint -v 2.5.0',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
